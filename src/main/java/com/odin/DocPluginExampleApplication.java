f12b2bf5bbcf12e91e2147804622e3b0f33c3a15
Caleb Schmitt <Caleb Schmitt>
2017-04-26 18:35:34 -0500

    Initial commit

---
+++ src/main/java/com/odin/DocPluginExampleApplication.java
@@ -0,0 +1,12 @@
+package com.odin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocPluginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocPluginExampleApplication.class, args);
+	}
+}
